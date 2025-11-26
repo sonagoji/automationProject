0dd6b4ee3d0dfdada047e06403ffa928231eb9b1
sonagoji <sonagoji1978@gmail.com>
2025-11-26 16:34:54 +0530

    Updated testrunner and reports

--- src/test/java/testrunner.java
+++ src/test/java/testrunner.java
@@ -1,7 +1,7 @@
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features="features",glue= {"stepdefinition"})
+@CucumberOptions(features="features",glue= {"stepdefinition"},plugin = {"pretty", "html:ReportsFolder/cucumber_reports.html"},monochrome=true)
 
 public class testrunner extends AbstractTestNGCucumberTests{
 
